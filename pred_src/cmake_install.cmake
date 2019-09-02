59340515f11a66316fe42c694f1f14521b6f5320
Hector Acosta <hector.acosta.pozo@outlook.com>
2019-09-02 10:36:39 -0400

    Using flask

--- pred_src/cmake_install.cmake
+++ pred_src/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/hacosta/Documents/Projects/predictor/pred_src
+# Install script for directory: /home/hacosta/Documents/Projects/predictor/wind-predictor/pred_src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/hacosta/Documents/Projects/predictor/pred_src/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/hacosta/Documents/Projects/predictor/wind-predictor/pred_src/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- server.py
+++ server.py
@@ -1,45 +1,105 @@
-import http.server
-import requests
-import os
-from urllib.parse import unquote, parse_qs
-import threading
-from socketserver import ThreadingMixIn
+from flask import Flask, escape, request, send_from_directory
 import subprocess
+import demjson
+import requests
+import tempfile
+import json
 
-class ThreadHTTPServer(ThreadingMixIn, http.server.HTTPServer):
-    "This is an HTTPServer that supports thread-based concurrency."
+app = Flask(__name__)
 
-class RequestHandler(http.server.SimpleHTTPRequestHandler):
+def form_to_ini(form):
+    """Take the form submitted and convert it to an INI file."""
+    ini_dict = {}
 
-    def end_headers (self):
-        self.send_header('Access-Control-Allow-Origin', '*')
-        http.server.SimpleHTTPRequestHandler.end_headers(self)
+    for k in form.keys():
+        v = form[k]
+        sec_key = k.split(':')
+        if len(sec_key) != 2:
+            continue
+        if not sec_key[0] in ini_dict:
+            ini_dict[sec_key[0]] = {sec_key[1]: v}
+        else:
+            ini_dict[sec_key[0]][sec_key[1]] = v
 
-    def do_POST(self):
-        length = self.headers.get('content-length')
-        content_type = self.headers.get('content-type')
-        nbytes = int(length)
-        data = self.rfile.read(nbytes)
+    ini = ''
+    for (k, v) in ini_dict.items():
+        ini = ini + '[%s]\n' % k
+        for (k2, v2) in v.items():
+            ini = ini + '%s = %s\n' % (k2, v2)
 
-        path = self.path
-        file_name = self.translate_path(path)
+    return ini
 
-        cmd = [file_name]
+def dict_to_binary(the_dict):
+    str = json.dumps(the_dict)
+    binary = ' '.join(format(ord(letter), 'b') for letter in str)
+    return binary
 
-        if content_type == 'application/x-www-form-urlencoded':
-            cmd.append(data)
+@app.route('/status/<string:target>')
+def show_target(target):
+    return send_from_directory(target, 'progress.json')
 
-        p = subprocess.Popen(cmd, stdin=subprocess.PIPE,
-                                    stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE)
-        output, err = p.communicate()
-        self.send_response(200, 'Here comes the script output!')
-        self.wfile.write(output)
-        print(output)
-        p.stdout.close()
-        p.stderr.close()
+@app.route('/winddata/pull', methods=['POST'])
+def import_wind_data():
+    file_name = 'PROGRESS_{0}_{1}_{2}'.format(
+                request.form['launch-site:timestamp'],
+                request.form['launch-site:latitude'],
+                request.form['launch-site:longitude']
+            )
 
-if __name__ == '__main__':
-    server_address = ('', int(os.environ.get('PORT', '3000')))
-    httpd = ThreadHTTPServer(server_address, RequestHandler)
-    httpd.serve_forever()
+    subprocess.Popen(
+        (['python', './predict.py', '--cd=./', '--fork', '--alarm', '-v', '-p1', '-f5', '--latdelta=2', '--londelta=2',
+            '-t {0}'.format(request.form['launch-site:timestamp']),
+            '--lat={0}'.format(request.form['launch-site:latitude']),
+            '--lon={0}'.format(request.form['launch-site:longitude']),
+            file_name
+        ]),
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE
+    )
+
+    output_dict = {
+        'log': 'Request sent',
+        'output': file_name
+    }
+    output_json = demjson.encode(output_dict)
+    return output_json
+
+@app.route('/predict', methods=['POST'])
+def get_prediction():
+    ini = form_to_ini({
+        'launch-site:latitude': request.form['launch-site:latitude'],
+        'launch-site:longitude': request.form['launch-site:longitude'],
+        'launch-site:altitude': request.form['launch-site:altitude'],
+        'launch-site:timestamp': request.form['launch-site:timestamp'],
+        'atmosphere:wind-error': request.form['atmosphere:wind-error'],
+        'altitude-model:ascent-rate': request.form['altitude-model:ascent-rate'],
+        'altitude-model:descent-rate': request.form['altitude-model:descent-rate'],
+        'altitude-model:burst-altitude': request.form['altitude-model:burst-altitude']
+    })
+
+    pred_process = subprocess.Popen(
+        (['./pred_src/pred', '-v', '-i', './gfs/']),
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    (output, log) = pred_process.communicate(bytes(ini, 'utf-8'))
+
+    parsed_output = []
+    for output_line in output.decode('utf-8').split('\n'):
+        if len(output_line) == 0:
+            continue
+
+        output_fields = output_line.rstrip().split(',')
+        if len(output_fields) < 2:
+            continue
+
+        parsed_output.append(output_fields)
+
+    output_dict = {
+        'log': filter(lambda x: x.rstrip(), log.decode('utf-8').split('\n')),
+        'output': parsed_output
+    }
+    output_json = demjson.encode(output_dict)
+        
+    return output_json
+
+
+if __name__ == "__main__":
+    app.run()
